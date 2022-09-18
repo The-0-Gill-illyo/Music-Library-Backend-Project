cc74d2888bbdbf92cd54e19c1ddff8942e5c5935
Theo Gill <theogill1369@gmail.com>
2022-09-18 12:24:45 -0400

    Created Music_LibrarySerializer class

---
+++ music_library/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Music_Library
+
+class Music_LibrarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Music_Library
+        fields = ["id, title, artist, album, release_date, genre"]
