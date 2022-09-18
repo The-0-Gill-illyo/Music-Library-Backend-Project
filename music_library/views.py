be76dfcbc3715af17cc01e30ca853969bdac8394
Theo Gill <theogill1369@gmail.com>
2022-09-18 16:55:38 -0400

    Created POST request functionality + made code easier to read

--- music_library/views.py
+++ music_library/views.py
@@ -1,15 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import Music_LibrarySerializer
 from .models import Music_Library
 
 # Create your views here.
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def music_list(request):
 
-    QuerySet = Music_Library.objects.all()
+    if request.method == 'GET':
+        lists = Music_Library.objects.all()
+        serializer = Music_LibrarySerializer(lists, many=True)
+        return Response(serializer.data)
 
-    serializer = Music_LibrarySerializer(QuerySet, many=True)
-
-    return Response(serializer.data)
-
+    elif request.method == 'POST':
+        serializer = Music_LibrarySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
