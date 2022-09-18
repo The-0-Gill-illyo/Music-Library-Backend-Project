9dae40fb7d23c24e290f5b2f3b5c750b5441b0f9
Theo Gill <theogill1369@gmail.com>
2022-09-18 17:37:14 -0400

    Created new songs id view path + added url <int:pk> in urls file

--- music_library/views.py
+++ music_library/views.py
@@ -7,14 +7,22 @@ from .models import Music_Library
 # Create your views here.
 @api_view(['GET', 'POST'])
 def music_list(request):
-
     if request.method == 'GET':
         lists = Music_Library.objects.all()
         serializer = Music_LibrarySerializer(lists, many=True)
         return Response(serializer.data)
-
     elif request.method == 'POST':
         serializer = Music_LibrarySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def song_details(request, pk):
+    try:
+        song = Music_Library.objects.get(pk=pk)
+        serializer = Music_LibrarySerializer(song);
+
+        return Response(serializer.data)
+    except Music_Library.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
