cb9904c196266d84e9f237587eafe49990ad1a85
Theo Gill <theogill1369@gmail.com>
2022-09-18 18:02:37 -0400

    Created 'DELETE' function in music_library app

--- music_library/views.py
+++ music_library/views.py
@@ -19,7 +19,7 @@ def music_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def song_details(request, pk):
     song = get_object_or_404(Music_Library, pk=pk)
     if request.method == 'GET':
@@ -31,3 +31,6 @@ def song_details(request, pk):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
