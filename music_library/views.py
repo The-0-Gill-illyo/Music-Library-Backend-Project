04bc4de387d24330ba562945109014b393133918
Theo Gill <theogill1369@gmail.com>
2022-09-18 13:47:19 -0400

    created list objects and serializer

--- music_library/views.py
+++ music_library/views.py
@@ -1,11 +1,14 @@
-import imp
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import Music_LibrarySerializer
+from .models import Music_Library
 
 # Create your views here.
 @api_view(['GET'])
 def music_list(request):
+    lists = Music_Library.objects.all()
 
+    serializer = Music_LibrarySerializer(lists, many=True)
 
-    return Response("ok")
+    return Response(serializer.data)
 
